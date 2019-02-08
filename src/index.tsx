755c9ca5209b92dee3691112a5f22e7cbeb3bf00
Ahmad Kanawi <ahmad@mosey.systems>
2019-02-07 22:57:11 -0800

    various ux improvements

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.scss'
 
+declare var window: Window & {
+  testing: boolean
+}
+
 const App = () => (
   <>
     <header>
       <h2>Enter a zip code to find some Hams!</h2>
     </header>
     <main>
-      <h6>To narrow to Zip+4, omit the dash (e.g. 920563043)</h6>
       <HammerSearch />
     </main>
     <footer>
@@ -28,57 +31,80 @@ interface State {
   fetchError?: string
   inputError: boolean
   hammers?: Hammer[]
-  value: string
+  working: boolean
+  inputValue: string
 }
 class HammerSearch extends React.Component {
-  public state: State = { inputError: false, value: '' }
+  public state: State = { inputError: false, inputValue: '', working: false }
   public render() {
     return (
       <>
         <form onSubmit={this.getEm}>
           <input
-            value={this.state.value}
-            onChange={(e) => this.setState({ value: e.currentTarget.value })}
+            value={this.state.inputValue}
+            type="search"
+            onChange={(e) =>
+              this.setState({ inputValue: e.currentTarget.value })
+            }
+            // pattern="\d{9}"
           />
           <button>Search</button>
         </form>
+        <h6>To narrow to Zip+4, omit the dash! (e.g. )</h6>
+
         {(this.state.inputError || this.state.fetchError) && (
           <div className="errors">
             {this.state.fetchError}
-            {this.state.inputError && "That's doesn't look like a zip code..."}
+            {this.state.inputError && "That doesn't look like a zip code..."}
           </div>
         )}
-        <div className="hammer-table">
-          <table>
-            <tbody>
-              {this.state.hammers &&
-                this.state.hammers.map((hammer) => (
-                  <tr>
-                    <td>{hammer.calls.join(', ')}</td>
-                    <td>{hammer.name}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
+        {this.state.working && (
+          <span role="img" className="loading">
+            📻
+          </span>
+        )}
+        {this.state.hammers &&
+          (this.state.hammers.length ? (
+            <div className="hammer-table">
+              <table>
+                <tbody>
+                  {this.state.hammers &&
+                    this.state.hammers.map((hammer) => (
+                      <tr key={hammer.calls.join('')}>
+                        <td>{hammer.calls.join(', ')}</td>
+                        <td>{hammer.name}</td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <div className="errors">No hams found with that zip!</div>
+          ))}
       </>
     )
   }
 
   private getEm = async (e: React.SyntheticEvent) => {
     e.preventDefault()
-    if (Number.isNaN(Number(this.state.value))) {
+    this.setState({ fetchError: undefined })
+    if (
+      Number.isNaN(Number(this.state.inputValue)) ||
+      this.state.inputValue.length < 5 ||
+      this.state.inputValue.length > 9
+    ) {
       this.setState({ inputError: true })
       return
     }
 
-    this.setState({ inputError: false })
+    this.setState({ hammers: undefined, inputError: false, working: true })
     let fetchError = 'Error fetching Hams :,('
     try {
-      const res = await fetchHams(this.state.value)
+      const res = await fetchHams(this.state.inputValue)
       if (res.status === 200) {
         const hammers = await res.json()
-        this.setState({ hammers, fetchError: undefined })
+        fetchError = undefined
+        this.setState({ hammers })
         return
       } else if (res.status === 429) {
         fetchError = 'Slow down, cowboy ;P'
@@ -87,16 +113,16 @@ class HammerSearch extends React.Component {
       }
     } catch (err) {
       // tslint:disable-next-line:no-console
-      console.error(err)
+      // console.error(err)
+    } finally {
+      this.setState({ fetchError, working: false })
     }
-
-    this.setState({ fetchError })
   }
 }
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
 
 const fetchHams = async (zip: string) => {
-  return fetch('/api', {
+  return fetch(window.testing ? 'http://localhost:7343' : '/api', {
     body: JSON.stringify({ zip }),
     headers: {
       'Content-Type': 'application/json'
