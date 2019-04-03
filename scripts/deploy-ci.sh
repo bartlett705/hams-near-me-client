942ed0ed3eb73015a73e649a1d7e8dc05503b3ba
Ahmad Kanawi <ahmad@mosey.systems>
2019-04-02 20:49:22 -0700

    adds deploy config

--- scripts/deploy-ci.sh
+++ scripts/deploy-ci.sh
@@ -1,3 +1,4 @@
+#
 echo "Starting Decrypt..."
 openssl enc -aes-256-cbc -d -in secrets.tar.enc -out secrets.tar -K $CONFIG_KEY -iv $CONFIG_IV
 echo "Extracting..."
@@ -6,7 +7,7 @@ ls -al ci
 echo "Decrpyt Done!"
 
 echo "Building Dist Pkg..."
-npm run build && echo "Copying Dist to remote host..." && scp -v -P 5235 -i ci/travis_id_rsa -o UserKnownHostsFile=ci/known_hosts build/. travis@mosey.systems:/var/www/undefined/build
+npm run build && echo "Copying Dist Pkg to remote host..." && scp -v -P 5235 -i ci/travis_id_rsa -o UserKnownHostsFile=ci/known_hosts build/. travis@mosey.systems:/var/www/hams-near-me/client/build
 echo "Cleaning up..."
-rm -rf build
+rm -rf build ci/travis_id_rsa
 echo Done!
